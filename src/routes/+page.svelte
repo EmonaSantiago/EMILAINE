57c661a2d8ad7e80acb980a1df8e2be84e85c6e0
Emona Santiago <emonasantiago1207@gmail.com>
2024-08-30 22:11:48 +0200

    basics

---
+++ src/routes/+page.svelte
@@ -0,0 +1,78 @@
+
+
+  <body>
+  
+  <h1>ATE EMEYS</h1>
+  <p>handmade custom crozier </p>
+  
+  <div class="row">
+    <div class="column">
+      <div class="card">
+        <h3>Card 1</h3>
+        <p>Some text</p>
+        <p>Some text</p>
+      </div>
+    </div>
+  
+    <div class="column">
+      <div class="card">
+        <h3>Card 2</h3>
+        <p>Some text</p>
+        <p>Some text</p>
+      </div>
+    </div>
+    
+    <div class="column">
+      <div class="card">
+        <h3>Card 3</h3>
+        <p>Some text</p>
+        <p>Some text</p>
+      </div>
+    </div>
+    
+    <div class="column">
+      <div class="card">
+        <h3>Card 4</h3>
+        <p>Some text</p>
+        <p>Some text</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- modify this form HTML and place wherever you want your form -->
+
+
+    <form
+action="https://formspree.io/f/xwpeyjad"
+method="POST"
+>
+<h2> ORDER HERE !!!! </h2>
+      <label for="fname">First Name</label>
+      <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+  
+      <label for="lname">Last Name</label>
+      <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+      <label for="lname">Phone Number
+      </label>
+      <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+  
+      <!-- <label for="country">Country</label>
+      <select id="country" name="country">
+        <option value="australia">Australia</option>
+        <option value="canada">Canada</option>
+        <option value="usa">USA</option>
+      </select> -->
+
+      <label>
+  Your Order:
+  <textarea name="message"></textarea>
+</label>
+    
+      <input type="submit" value="SUBMIT">
+    </form>
+
+  
+  </body>
+
+  
